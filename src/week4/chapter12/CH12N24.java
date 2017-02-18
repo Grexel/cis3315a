a1eeef315d4ccce4c5438e63d07f245d653f2325
Jeffrey Miller <millerjl07@mounties.mansfield.edu>
2017-02-18 16:43:26 -0500

    Ch12N24

---
+++ src/week4/chapter12/CH12N24.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4.chapter12;
+
+/**
+ * Create a large dataset
+ * @author JeffreyMiller
+ */
+public class CH12N24 {
+    public static void main(String[] args) {
+        
+    }
+    public static String createAssistant(){
+        return "";
+    }
+}
