d15d905c63e480653009e58879776ce29fe195bd
Jeffrey Miller <millerjl07@mounties.mansfield.edu>
2017-02-20 21:15:23 -0500

    CH13N17 Complex calculator working

---
+++ src/week4/CH13N17/Complex.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4.CH13N17;
+
+/**
+ *
+ * @author JeffreyMiller
+ */
+public class Complex {
+    private double realPart;
+    //the b of bi where i = sqrt(-1)
+    private double imaginaryPart;
+
+    public Complex() {
+        this.realPart = 0;
+        this.imaginaryPart = 0;
+    }
+    public Complex(double real) {
+        this.realPart = real;
+        this.imaginaryPart = 0;
+    }
+
+    public Complex(double real, double imaginary) {
+        this.realPart = real;
+        this.imaginaryPart = imaginary;
+    }
+    public Complex add(Complex complex2){
+        double newReal = this.getRealPart() + complex2.getRealPart();
+        double newImaginary = this.getImaginaryPart() + complex2.getImaginaryPart();
+        return new Complex(newReal,newImaginary);
+    }
+    public Complex subtract(Complex complex2){
+        double newReal = this.getRealPart() - complex2.getRealPart();
+        double newImaginary = this.getImaginaryPart() - complex2.getImaginaryPart();
+        return new Complex(newReal,newImaginary);
+    }
+    public Complex multiply(Complex complex2){
+        double newReal = this.getRealPart() * complex2.getRealPart() 
+                - this.getImaginaryPart()*complex2.getImaginaryPart();
+        double newImaginary = this.getRealPart() * complex2.getImaginaryPart()
+                + this.getImaginaryPart() * complex2.getRealPart();
+        return new Complex(newReal,newImaginary);
+    }
+    public Complex divide(Complex complex2){
+        double cdSquare = complex2.getRealPart() * complex2.getRealPart()
+                + complex2.getImaginaryPart() * complex2.getImaginaryPart();
+        
+        double newReal = (this.getRealPart() * complex2.getRealPart() 
+                + this.getImaginaryPart()*complex2.getImaginaryPart()) / cdSquare;
+        double newImaginary = (this.getImaginaryPart() * complex2.getRealPart()
+                - this.getRealPart() * complex2.getImaginaryPart()) / cdSquare;
+        return new Complex(newReal,newImaginary);
+    }
+    public double absolute(){
+        return Math.sqrt(this.getRealPart()*this.getRealPart() 
+                + this.getImaginaryPart() * this.getImaginaryPart());
+    }
+    public double getRealPart() {
+        return realPart;
+    }
+
+    public void setRealPart(double realPart) {
+        this.realPart = realPart;
+    }
+
+    public double getImaginaryPart() {
+        return imaginaryPart;
+    }
+
+    public void setImaginaryPart(double imaginaryPart) {
+        this.imaginaryPart = imaginaryPart;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getRealPart() + " + " + getImaginaryPart() + "i)";
+    }
+    
+}
---
+++ src/week4/CH13N17/ComplexCalculator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4.CH13N17;
+
+import java.util.Scanner;
+import week4.CH13N16.Rational;
+
+/**
+ *
+ * @author JeffreyMiller
+ */
+public class ComplexCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double real1, imaginary1;
+        double real2, imaginary2;
+        System.out.println("Complex Calculator");
+        System.out.println("Enter a complex problem to solve as (real imaginary)");
+        System.out.println("such as 3.5 5.5 for 3.5 + 5.5i");
+        System.out.print("Enter the first complex number: ");
+        real1 = sc.nextDouble();
+        imaginary1 = sc.nextDouble();
+        System.out.print("Enter the second complex number: ");
+        real2 = sc.nextDouble();
+        imaginary2 = sc.nextDouble();
+        Complex complex1 = new Complex(real1, imaginary1);
+        Complex complex2 = new Complex(real2, imaginary2);
+        System.out.println(complex1 + " + " + complex2 + " = " + complex1.add(complex2));
+        System.out.println(complex1 + " - " + complex2 + " = " + complex1.subtract(complex2));
+        System.out.println(complex1 + " * " + complex2 + " = " + complex1.multiply(complex2));
+        System.out.println(complex1 + " / " + complex2 + " = " + complex1.divide(complex2));
+        System.out.println("|" + complex1 + "| = " + complex1.absolute());
+    }
+}
